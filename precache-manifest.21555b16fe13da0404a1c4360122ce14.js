18d18f9e777bac602713a2f1956861d828549055
Maksym Kovalenko <m.v.kovalenko4@gmail.com>
2020-11-01 00:49:12 +0200

    Updates

---
+++ precache-manifest.21555b16fe13da0404a1c4360122ce14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c262ddaca568c8b327c767a28d3a0d",
+    "url": "/phonebook-6-react/index.html"
+  },
+  {
+    "revision": "e99e13ec65b8527ab122",
+    "url": "/phonebook-6-react/static/css/main.4cba9ae3.chunk.css"
+  },
+  {
+    "revision": "af9d5a50ed7dadbf68b8",
+    "url": "/phonebook-6-react/static/js/2.2e421e1c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/phonebook-6-react/static/js/2.2e421e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99e13ec65b8527ab122",
+    "url": "/phonebook-6-react/static/js/main.86a3eaf6.chunk.js"
+  },
+  {
+    "revision": "e6fd4c8e1a49e259ba79",
+    "url": "/phonebook-6-react/static/js/runtime-main.34c0b909.js"
+  }
+]);
